f7ce5f5802f7f734951238477073df2d2647d7d6
ezl-13 <ericzlou@gmail.com>
2020-04-08 22:45:55 -0400

    Initial site

---
+++ js/dashboard.js
@@ -0,0 +1,234 @@
+const base_url = "https://www.microprediction.org/home/"
+
+const response = {
+  "code": "f1a76a7570a77514ac6e63405340c4b4",
+  "animal": "Flathat Stoat",
+  "balance::74ab5a2c6205469402cb94d2935d3443.json": -20.789482830058834,
+  "distance_to_bankruptcy": 655339.2105171699,
+  "/active/74ab5a2c6205469402cb94d2935d3443": [
+    "70::z1~covid_news_nyc~310.json",
+    "70::covid_news_nyc.json",
+    "310::hospital_bike_activity.json",
+    "70::z1~covid_news_nyc~910.json",
+    "70::z1~hospital_bike_activity~310.json",
+    "70::z1~hospital_bike_activity~910.json",
+    "70::z1~covid_news_nyc~70.json",
+    "910::z1~covid_news_nyc~310.json",
+    "910::covid_news_nyc.json",
+    "70::hospital_bike_activity.json"
+  ],
+  "performance::74ab5a2c6205469402cb94d2935d3443.json": {
+    "910::covid_news_nyc.json": 1.3981013333333334,
+    "70::covid_news_nyc.json": 1.3544106666666669,
+    "310::covid_news_nyc.json": 1.048576,
+    "910::z1~bart_delays~70.json": 0.9611946666666668,
+    "70::z1~bart_delays~70.json": 0.9175040000000002,
+    "310::z1~hospital_bike_activity~310.json": 0.8738133333333333,
+    "70::z1~hospital_bike_activity~70.json": 0.7084129523809523,
+    "310::z1~covid_news_nyc~310.json": 0.577220923076923,
+    "910::z1~bart_delays~310.json": 0.5679786666666667,
+    "70::z1~hospital_bike_activity~310.json": 0.48059733333333327,
+    "310::z1~bart_delays~910.json": 0.34952533333333324,
+    "310::hospital_bike_activity.json": 0.3058346666666665,
+    "910::hospital_bike_activity.json": 0.21845333333333328,
+    "70::z1~covid_news_nyc~310.json": 0.17476266666666687,
+    "910::z1~hospital_bike_activity~310.json": 0.17476266666666657,
+    "70::z1~covid_news_nyc~70.json": -0.04369066666666659,
+    "70::bart_delays.json": -0.13107200000000002,
+    "910::z1~covid_news_nyc~310.json": -0.5024426666666667,
+    "70::z1~hospital_bike_activity~910.json": -0.5679786666666667,
+    "70::hospital_bike_activity.json": -0.6054278095238097,
+    "70::z1~covid_news_nyc~910.json": -0.6335146666666667,
+    "310::z1~covid_news_nyc~910.json": -0.65536,
+    "910::z1~covid_news_nyc~70.json": -0.692809142857143,
+    "910::z1~hospital_bike_activity~70.json": -0.7427413333333334,
+    "70::z1~bart_delays~910.json": -0.7427413333333334,
+    "310::z1~hospital_bike_activity~910.json": -0.7864320000000001,
+    "910::z1~hospital_bike_activity~910.json": -0.8221789090909092,
+    "310::z1~covid_news_nyc~70.json": -0.9266698741258744,
+    "310::bart_delays.json": -0.9889978181818183,
+    "310::z1~bart_delays~70.json": -1.0048853333333334,
+    "910::z1~covid_news_nyc~910.json": -1.185889523809524,
+    "70::z1~bart_delays~310.json": -1.2233386666666668,
+    "310::z1~hospital_bike_activity~70.json": -1.4199466666666667,
+    "910::bart_delays.json": -1.5291733333333333,
+    "910::z1~bart_delays~910.json": -2.228224,
+    "310::z1~bart_delays~310.json": -2.3156053333333335
+  },
+  "confirms::74ab5a2c6205469402cb94d2935d3443.json": [
+    "{\"time\": \"2020-04-09 01:26:39.973770\", \"epoch_time\": 1586395599.9737792, \"operation\": \"set\", \"count\": 1, \"examples\": [{\"name\": \"hospital_bike_activity.json\", \"write_key\": \"74ab5a2c6205469402cb94d2935d3443\", \"value\": \"11.0\", \"percentiles\": {\"0\": 0.22550000000000003, \"1\": 0.22450000000000003, \"2\": 0.5}}]}",
+    "{\"time\": \"2020-04-09 01:26:39.972651\", \"epoch_time\": 1586395599.9726613, \"operation\": \"set\", \"count\": 3, \"examples\": [{\"name\": \"z1~hospital_bike_activity~70.json\", \"write_key\": \"74ab5a2c6205469402cb94d2935d3443\", \"value\": -0.7537489216716375, \"percentiles\": {\"0\": 0.5, \"1\": 0.5, \"2\": 0.5}}, {\"name\": \"z1~hospital_bike_activity~310.json\", \"write_key\": \"74ab5a2c6205469402cb94d2935d3443\", \"value\": -0.7570832306538241, \"percentiles\": {\"0\": 0.5, \"1\": 0.5, \"2\": 0.5}}]}",
+    "{\"time\": \"2020-04-09 01:26:39.966747\", \"epoch_time\": 1586395599.9667606, \"operation\": \"submit\", \"name\": \"z1~hospital_bike_activity~910.json\", \"success\": true, \"warn\": false, \"delays\": [310], \"some_values\": [-1.6828911137420235, -1.6800851219323705, -1.6799120812701793, -1.6784967772126584, -1.677388630252614]}",
+    "{\"time\": \"2020-04-09 01:26:39.944769\", \"epoch_time\": 1586395599.9447792, \"operation\": \"submit\", \"name\": \"z1~hospital_bike_activity~310.json\", \"success\": true, \"warn\": false, \"delays\": [910], \"some_values\": [-2.7506441653063742, -2.7487888394787, -2.7487433922058795, -2.7474628782935597, -2.74696573610623]}",
+    "{\"time\": \"2020-04-09 01:26:39.923192\", \"epoch_time\": 1586395599.9232028, \"operation\": \"submit\", \"name\": \"z1~hospital_bike_activity~310.json\", \"success\": true, \"warn\": false, \"delays\": [310], \"some_values\": [-2.745106671929359, -2.744242254315161, -2.7440643628031336, -2.7439007451792863, -2.743815332644591]}",
+    "{\"time\": \"2020-04-09 01:26:39.901385\", \"epoch_time\": 1586395599.9013956, \"operation\": \"submit\", \"name\": \"z1~hospital_bike_activity~310.json\", \"success\": true, \"warn\": false, \"delays\": [70], \"some_values\": [-2.7562691957903653, -2.7493959208133, -2.7487590137818283, -2.7486308991548922, -2.7465443585919385]}",
+    "{\"time\": \"2020-04-09 01:26:39.878388\", \"epoch_time\": 1586395599.878399, \"operation\": \"submit\", \"name\": \"z1~hospital_bike_activity~70.json\", \"success\": true, \"warn\": false, \"delays\": [910], \"some_values\": [-2.729967680452827, -2.7283287388365647, -2.728307467132374, -2.727110884161588, -2.72701742496075]}",
+    "{\"time\": \"2020-04-09 01:26:39.856272\", \"epoch_time\": 1586395599.8562837, \"operation\": \"submit\", \"name\": \"z1~hospital_bike_activity~70.json\", \"success\": true, \"warn\": false, \"delays\": [310], \"some_values\": [-2.7316560402494137, -2.727586922872038, -2.7264646206833745, -2.725970861605007, -2.7249863520791724]}",
+    "{\"time\": \"2020-04-09 01:26:39.832244\", \"epoch_time\": 1586395599.8322568, \"operation\": \"submit\", \"name\": \"z1~hospital_bike_activity~70.json\", \"success\": true, \"warn\": false, \"delays\": [70], \"some_values\": [-2.73326813701483, -2.72848789791279, -2.7274299315860295, -2.7273453726260515, -2.7268068075586553]}",
+    "{\"time\": \"2020-04-09 01:26:39.796262\", \"epoch_time\": 1586395599.7962756, \"operation\": \"submit\", \"name\": \"hospital_bike_activity.json\", \"success\": true, \"warn\": false, \"delays\": [910], \"some_values\": [-159.0006643091648, -158.99973954723575, -158.9978767246154, -158.9955985727298, -158.9950625768973]}"
+  ],
+  "errors::74ab5a2c6205469402cb94d2935d3443.json": [],
+  "warnings::74ab5a2c6205469402cb94d2935d3443.json": [],
+  "transactions::74ab5a2c6205469402cb94d2935d3443.json": [
+    [
+      "1586395582636-0",
+      {
+        "settlement_time": "2020-04-09 01:26:22.637793",
+        "stream": "z1~covid_news_nyc~70.json",
+        "delay": "70",
+        "stream_owner_code": "f1a76a7570a77514ac6e63405340c4b4",
+        "recipient_code": "f1a76a7570a77514ac6e63405340c4b4",
+        "amount": "0.39321600000000007"
+      }
+    ],
+    [
+      "1586393172299-0",
+      {
+        "settlement_time": "2020-04-09 00:46:12.299739",
+        "stream": "z1~covid_news_nyc~910.json",
+        "delay": "310",
+        "stream_owner_code": "f1a76a7570a77514ac6e63405340c4b4",
+        "recipient_code": "f1a76a7570a77514ac6e63405340c4b4",
+        "amount": "-0.2184533333333334"
+      }
+    ],
+    [
+      "1586379940567-0",
+      {
+        "settlement_time": "2020-04-08 21:05:40.560099",
+        "stream": "z1~hospital_bike_activity~910.json",
+        "delay": "310",
+        "stream_owner_code": "f1a76a7570a77514ac6e63405340c4b4",
+        "recipient_code": "f1a76a7570a77514ac6e63405340c4b4",
+        "amount": "-0.131072"
+      }
+    ],
+    [
+      "1586379915421-0",
+      {
+        "settlement_time": "2020-04-08 21:05:15.414288",
+        "stream": "z1~covid_news_nyc~310.json",
+        "delay": "310",
+        "stream_owner_code": "f1a76a7570a77514ac6e63405340c4b4",
+        "recipient_code": "f1a76a7570a77514ac6e63405340c4b4",
+        "amount": "-0.2184533333333334"
+      }
+    ],
+    [
+      "1586377508294-0",
+      {
+        "settlement_time": "2020-04-08 20:25:08.287853",
+        "stream": "z1~covid_news_nyc~70.json",
+        "delay": "310",
+        "stream_owner_code": "f1a76a7570a77514ac6e63405340c4b4",
+        "recipient_code": "f1a76a7570a77514ac6e63405340c4b4",
+        "amount": "-0.05957818181818177"
+      }
+    ],
+    [
+      "1586376320644-0",
+      {
+        "settlement_time": "2020-04-08 20:05:20.637851",
+        "stream": "hospital_bike_activity.json",
+        "delay": "310",
+        "stream_owner_code": "f1a76a7570a77514ac6e63405340c4b4",
+        "recipient_code": "f1a76a7570a77514ac6e63405340c4b4",
+        "amount": "0.2184533333333333"
+      }
+    ],
+    [
+      "1586355576680-0",
+      {
+        "settlement_time": "2020-04-08 14:19:36.679919",
+        "stream": "z1~bart_delays~910.json",
+        "delay": "70",
+        "stream_owner_code": "f1a76a7570a77514ac6e63405340c4b4",
+        "recipient_code": "f1a76a7570a77514ac6e63405340c4b4",
+        "amount": "-0.04369066666666666"
+      }
+    ],
+    [
+      "1586344226000-0",
+      {
+        "settlement_time": "2020-04-08 11:10:25.997681",
+        "stream": "z1~hospital_bike_activity~910.json",
+        "delay": "910",
+        "stream_owner_code": "f1a76a7570a77514ac6e63405340c4b4",
+        "recipient_code": "f1a76a7570a77514ac6e63405340c4b4",
+        "amount": "-0.29789090909090915"
+      }
+    ],
+    [
+      "1586340661109-0",
+      {
+        "settlement_time": "2020-04-08 10:11:01.105239",
+        "stream": "covid_news_nyc.json",
+        "delay": "910",
+        "stream_owner_code": "f1a76a7570a77514ac6e63405340c4b4",
+        "recipient_code": "f1a76a7570a77514ac6e63405340c4b4",
+        "amount": "0.39321600000000007"
+      }
+    ],
+    [
+      "1586339408564-0",
+      {
+        "settlement_time": "2020-04-08 09:50:08.559819",
+        "stream": "z1~hospital_bike_activity~310.json",
+        "delay": "910",
+        "stream_owner_code": "f1a76a7570a77514ac6e63405340c4b4",
+        "recipient_code": "f1a76a7570a77514ac6e63405340c4b4",
+        "amount": "-0.3058346666666667"
+      }
+    ]
+  ]
+}
+
+function LoadDashboard() {
+	var write_key = document.getElementById("box-input-write-key").value;
+	var url = base_url+write_key;
+
+	// const request = new Request(url, {method: 'GET'});
+
+	// fetch(request)
+	// 	.then(response => {
+	// 		console.log(response);
+	// 		if (response.status === 200) {
+	// 			;
+	// 		}
+	// 		else {
+	// 			console.log("ERROR");
+	// 			;
+	// 		}
+	// 	})
+	// 	.catch(error => {
+	// 		console.log(error);
+	// 	})
+	
+	LoadOverview();
+	LoadActiveStreams();
+	LoadPerformance();
+	LoadConfirms();
+	LoadTransactions();
+	LoadErrors();
+	LoadWarnings();
+}
+
+function CreateTitle(div, title) {
+	var title_div = document.createElement("div");
+	title_div.id = "dashboard-title";
+	title_div.innerHTML = title;
+	div.appendChild(title_div);
+	return div;
+}
+
+function LoadOverview() {
+	var dashboard_overview = document.getElementById("dashboard-overview");
+	dashboard_overview = CreateTitle(dashboard_overview, "Overview");
+	var code_div = document.createElement("div");
+	code_div.id = "body-text";
+	var text = response["code"];
+	code_div.innerHTML = text;
+	dashboard_overview.appendChild(code_div);
+
+	document.getElementById("dashboard-overview") = dashboard_overview;
+}
