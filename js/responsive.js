e933b5e6957dfb6e9dacc5a1c4f349ee901b0ab5
ezl-13 <ericzlou@gmail.com>
2020-04-26 22:09:04 -0400

    huge navbar changes

--- js/responsive.js
+++
@@ -1,9 +0,0 @@
-function resize_fn() {
-	var w = window.innerWidth;
-	var h = window.innerHeight;
-	if (w <= 1200) {
-		document.getElementById("body-title").innerText = "small";
-	} else {
-		document.getElementById("body-title").innerText = "big"
-	}
-}
