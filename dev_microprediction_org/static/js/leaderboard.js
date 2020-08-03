9e4ec3de471793b5eaf82583f17bc4008a367403
ezl-13 <ericzlou@gmail.com>
2020-08-03 00:21:26 -0400

    Show all leaderboards

--- dev_microprediction_org/static/js/leaderboard.js
+++ dev_microprediction_org/static/js/leaderboard.js
@@ -1,10 +1,49 @@
 const base_url = home_url;
+let leaderboard_dict = {};
+let nav_divs = [];
 
-async function LoadLeaderboard() {
-  const url = base_url + "overall/";
-  const dict = await get(url);
+async function LoadLeaderboardNav() {
 
+  let nav = document.getElementById("leaderboards-sidenav");
+
+  const prizes_url = base_url + "prizes/";
+  const prizes_dict = await get(prizes_url);
+  const overall_url = base_url + "overall/";
+
+  leaderboard_dict[overall_url] = [];
+  for (let url in prizes_dict)
+    leaderboard_dict[url] = prizes_dict[url];
+
+  // TODO: don't forget to use leaderboard_dict[x][1]
+  let divs_urls = [];
+  for (let url in leaderboard_dict) {
+    let nav_div = document.createElement("div");
+    nav_divs.push(nav_div);
+    if (url === "/overall/") {
+      nav_div.innerText = "Overall Leaderboard";
+      nav_div.classList.add("active-sidenav");
+    }
+    else
+      nav_div.innerText = url.substring(url.indexOf(".org/")+5);
+    nav.appendChild(nav_div);
+    nav_div.onclick = function() { 
+      LoadLeaderboard(url);
+      nav_div.classList.add("active-sidenav");
+      for (let t_div of nav_divs) {
+        if (t_div !== nav_div) 
+          t_div.classList.remove("active-sidenav");
+      }
+    };
+
+  }
+
+  LoadLeaderboard(base_url + "overall/");
+}
+
+async function LoadLeaderboard(url) {
+  let dict = await get(url);
   let leaderboard_div = document.getElementById("leaderboard");
+  leaderboard_div.innerHTML = "";
 
   // Empty leaderboard
   if (Object.keys(dict).length === 0) {
